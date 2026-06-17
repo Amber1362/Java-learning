707784ba82d6e2058b69a86f591e99bbcfc495e2
Amber Singh <ambersingh365@gmail.com>
2026-06-17 20:33:17 +0530

    Add primitive data types examples

---
+++ Core Java/Variables and Datatypes/PrimitiveTypes.java
@@ -0,0 +1,23 @@
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+       
+        byte age = 22;
+        short noOfStudents = 28132;
+        int populationOfDehradun = 12000000;
+        long worldPopulation = 8000000000L;
+        float price = 89.96f;
+        double percentile = 65.2;
+        char grade = 'A';
+        boolean isPlaced = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Number of students: " + noOfStudents);
+        System.out.println("Population of Dehradun: " + populationOfDehradun);
+        System.out.println("World population: " + worldPopulation);
+        System.out.println("Price: " + price);
+        System.out.println("Percentile: " + percentile);
+        System.out.println("Grade: " + grade);
+        System.out.println("Placed: " + isPlaced);
+
+    }
+}
