f02d332081c87cbd4673397f22b37a5664ca6cde
Amber Singh <ambersingh365@gmail.com>
2026-06-17 19:47:25 +0530

    Add variables and data types practice example

---
+++ Core Java/Variables and Datatypes/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String name = "Amber";
+        byte age = 20;
+        String course = "MCA";
+        double CGPA = 8.5;
+        boolean placementReady = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Course: " + course);
+        System.out.println("CGPA: " + CGPA);
+        System.out.println("Placement Ready: " + placementReady);
+    }
+}
