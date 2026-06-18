1252ac8f1f80cef7e2bcbfb1801be9fc4af092d0
Amber Singh <ambersingh365@gmail.com>
2026-06-18 10:47:06 +0530

    Add operators examples

---
+++ Core Java/Operators/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+
+        boolean isStudent = true;
+        boolean hasCertificate = false;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+        System.out.println(a > b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+
+        System.out.println(isStudent && hasCertificate);
+        System.out.println(isStudent || hasCertificate);
+        System.out.println(!isStudent);
+
+    }
+}
