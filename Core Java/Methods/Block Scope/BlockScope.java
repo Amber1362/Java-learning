d42f52990cad7e7e44b4163c47858066a8c1471e
Amber Singh <ambersingh365@gmail.com>
2026-06-22 13:56:38 +0530

    Practice method and block scope in Java

---
+++ Core Java/Methods/Block Scope/BlockScope.java
@@ -0,0 +1,16 @@
+public class BlockScope {
+    public static void main(String[] args) {
+
+        int age = 20;
+
+        if(age > 18) {
+            int vote = 1;
+
+            System.out.println(age);
+            System.out.println(vote);
+        }
+
+        System.out.println(age);
+        // System.out.println(vote);
+    }
+}
---
+++ Core Java/Methods/Method Scope/MethodScope.java
@@ -0,0 +1,10 @@
+public class MethodScope {
+    public static void showAge() {
+        int age = 20;
+        System.out.println("Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        showAge();
+    }
+}
